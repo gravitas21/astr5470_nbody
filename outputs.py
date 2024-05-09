e9e248978b0a33b1f8edadf4ce84df8c0a347b3b
gravitas21 <gravitas908@gmail.com>
2024-05-09 14:30:18 -0400

    Added function to save orbits

--- outputs.py
+++ outputs.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define plotting functions
-def plot_orbit_xyplane(positions,names):
+def plot_orbit_xyplane(positions,names,test=False):
     """
     Plot the orbit in X-Y plane
     """
@@ -20,7 +20,10 @@ def plot_orbit_xyplane(positions,names):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title("Your planetary system from the top")
-    plt.savefig("bound_orbit_xy.jpeg")
+    if not test:
+        plt.savefig("bound_orbit_xy.jpeg",bbox_inches='tight')
+    else:
+        plt.savefig('test_saved_orbits.jpg',bbox_inches='tight')
     plt.show()
 
 # Save integration results in output file
@@ -42,3 +45,61 @@ def save_orbits(outfile,times,names,M,positions,velocities):
                 f.write(",".join(str(item) for item in posvel)+"\n")
                 #print(p,v)
     print("Saved orbits to {}".format(outfile))
+
+# Read saved orbital parameters from outputfile
+def read_saved_orbits(outfile):
+    """
+    Reads the saved orbital positions and velocities from output file
+    """
+    with open(outfile,'r') as f:
+        print("Reading saved orbits from",outfile)
+        lines = f.readlines()
+        names = []
+        masses = []
+        times = []
+        ## Read the names and masses of all particles
+        firstline = lines[0]
+        labels = firstline[1:-1].split(',')
+        for label in labels:
+            name,mass = label.split('_')
+            names.append(name)
+            masses.append(float(mass))
+        N_bodies = len(labels)
+        print("Number of particles",N_bodies)
+        print("Names of particles",names)
+        print("Masses of particles",masses)
+        ## Determine the number of snapshots
+        snaps = 0
+        for ind,line in enumerate(lines):
+            if ind == 0:
+                continue
+            else:
+                if line[0] == '#':
+                    time = float(line[1:].split('=')[-1])
+                    times.append(time)
+                    snaps += 1
+        print("Number of snapshots",snaps)
+        positions = np.zeros((snaps,N_bodies,3))
+        velocities = np.zeros((snaps,N_bodies,3))
+        snapcounter = 0
+        counter = 0
+        for ind,line in enumerate(lines):
+            if ind == 0:
+                continue
+            else:
+                if line[0] == '#':
+                    continue
+                else:
+                    print(snapcounter,counter,'-->',line)
+                    posvel = np.array([float(p) for p in line[:-1].split(',')])
+                    print(posvel[0:3],posvel[3:6])
+                    positions[snapcounter,counter,:] = posvel[0:3]
+                    velocities[snapcounter,counter,:] = posvel[3:6]
+                    counter += 1
+            if counter > N_bodies - 1:
+                counter = 0
+                snapcounter += 1
+                #exit()
+        #print(N_bodies,names,masses,times)
+        #print(positions)
+        return N_bodies,names,masses,positions,velocities
