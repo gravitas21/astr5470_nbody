f34dfd51d4aff7f034e2f2cb7d65fd91a0e8a227
gravitas21 <gravitas908@gmail.com>
2024-05-09 01:43:13 -0400

    Updated TODO

--- nbody.py
+++ nbody.py
@@ -2,18 +2,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import integrators as intg
 from setup import read_input_file,read_command_line,run_Nbody
 from plots import plot_orbit_xyplane
 
 #TODO
 ## Document the code in README and create a Github wiki page
-## Implement the integrators
 ## Create output routines
 ## Implement three non-trivial tests
 
 # Defining main function
 def main():
+    """
+    Main function to run the astr_nbody code
+    Calls other functions to do each step
+    """
     ## Greet the user in Nepali!
     print("Namaste!")
 
@@ -22,7 +24,7 @@ def main():
 
     ## Read the input file: input.txt
     N_bodies,M,pos,vel,tend,delta_t,tframe,names = read_input_file(inputfile)
-    print(N_bodies,M,pos.shape,vel.shape,tend,delta_t,tframe)
+    print("Initial parameters:\n",N_bodies,M,pos.shape,vel.shape,tend,delta_t,tframe)
 
     ## Run the N-body integration
     positions, velocities, times = run_Nbody(N_bodies,M,pos,vel,tend,delta_t,tframe,integrator)
